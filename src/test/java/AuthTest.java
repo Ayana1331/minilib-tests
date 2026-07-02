ae0ad8976dc36234f79bcc1c722887d4328dac0d
Ayana1331 <bekzatovaayana6@gmail.com>
2026-07-03 01:09:37 +0600

    Add logout test

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -29,7 +29,7 @@ void loginReturns200() {
       .post("/auth/login")
       .then()
       .statusCode(200)
-      .body("token",notNullValue())
+      .body("token", notNullValue())
       .body("user_id", notNullValue())
       .body("expires_at", notNullValue());
 }
@@ -51,5 +51,34 @@ void loginWithWrongPasswordReturn400() {
       .then()
       .statusCode(401)
       .body("error", notNullValue());
-    }
+}
+
+@Test
+void logoutReturns204() {
+    String loginBody = """
+        {
+        "email":"admin@test.com",
+        "password":"admin123"
+        }
+      """;
+    
+    String token = given()
+                     .contentType("application/json")
+                     .body(loginBody)
+                     .when()
+                     .post("/auth/login")
+                     .then()
+                     .statusCode(200)
+                     .extract().path("token");
+    String userId = "16061ae8-abf6-4956-b855-326060d3a3a7";
+    
+    given()
+      .contentType("application/json")
+      .header("Authorization","Bearer "+token)
+      .body("{\"userId\":\""+userId+"\"}")
+      .when()
+      .post("/auth/logout")
+      .then()
+      .statusCode(204);
+}
 }
